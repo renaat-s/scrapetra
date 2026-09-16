c8bea5fd16262b587a75c8019425227ec441a862
Renaat <renaat.sibanda@gmail.com>
2026-09-16 23:47:55 +0100

    Switch from duckduckgo_search to ddgs (renamed package)

--- agent.py
+++ agent.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse, urljoin
 import aiodns
 import httpx
 from bs4 import BeautifulSoup
-from duckduckgo_search import DDGS
+from ddgs import DDGS
 
 
 EMAIL_REGEX = re.compile(
